573caf87569b13710215d4519461f7c750791dad
Gleb Storozhik <hello@noobusdeer.com>
2018-08-07 14:11:20 +0300

    switch to amperka tv out lib

--- Teleak.ino
+++ Teleak.ino
@@ -1,3 +1,11 @@
+#include <font4x6.h>
+#include <font6x8.h>
+#include <font8x8.h>
+#include <font8x8ext.h>
+#include <fontALL.h>
+#include <TVout.h>
+#include <video_gen.h>
+
 #include <TVout.h>
 #include <video_gen.h>
 
