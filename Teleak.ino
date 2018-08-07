69c42dc7e1793ae3676a5931e9b9845feb482d62
Gleb Storozhik <hello@noobusdeer.com>
2018-08-07 13:57:31 +0300

    add TVout library and .travis.yaml

--- Teleak.ino
+++ Teleak.ino
@@ -1,9 +1,10 @@
+#include <TVout.h>
+#include <video_gen.h>
+
 void setup() {
   // put your setup code here, to run once:
-
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
 }
